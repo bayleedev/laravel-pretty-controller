a2780eccd0fc530309bbe9200c05434228f889c7
Blaine Schmeisser <blainesch@gmail.com>
2014-06-08 22:07:50 -0500

    Remove MediaType defaults from app.

--- src/Blainesch/LaravelPrettyController/Http/MediaType.php
+++ src/Blainesch/LaravelPrettyController/Http/MediaType.php
@@ -4,32 +4,6 @@ namespace Blainesch\LaravelPrettyController\Http;
 
 use Negotiation\FormatNegotiator;
 
-MediaType::add('html', [
-	'conditions' => [
-		'accept' => [
-			'text/html',
-			'*/*',
-		],
-	],
-	'encode' => function($request, $response) {
-		$class = strtolower(str_replace('Controller', '', $request['controller']));
-		return \View::make("{$class}.{$request['method']}")->with($response);
-	},
-]);
-
-MediaType::add('json', [
-	'conditions' => [
-		'type' => 'json',
-		'accept' => [
-			'application/json',
-			'application/x-json',
-		],
-	],
-	'encode' => function($request, $response) {
-		return json_encode($response);
-	},
-]);
-
 class MediaType {
 
 	public $options = [];
