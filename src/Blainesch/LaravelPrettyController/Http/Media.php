3750fa94f1cb0fc38c6571e70a7a78647fbbc27d
Blaine Schmeisser <blainesch@gmail.com>
2014-06-08 21:41:12 -0500

    Delete legacy files. Move MediaTypes back to single file.

--- src/Blainesch/LaravelPrettyController/Http/Media.php
+++ src/Blainesch/LaravelPrettyController/Http/Media.php
@@ -2,54 +2,15 @@
 
 namespace Blainesch\LaravelPrettyController\Http;
 
-use Blainesch\LaravelPrettyController\Action\BadAcceptType;
-use Negotiation\FormatNegotiator;
 use App;
 
 class Media {
 
-	public $request = null;
-
-	public $response = null;
-
-	public function __construct($request, $response)
-	{
-		$this->request = $request;
-		$this->response = $response;
-	}
-
-	public function findByType()
-	{
-		foreach (MediaTypes::all() as $name => $mediaType) {
-			if ($mediaType->type() === $this->request['type']) {
-				return $mediaType;
-			}
-		}
-		return false;
-	}
-
-	public function findBestAccept()
-	{
-		$allAcceptTypes = MediaTypes::allAcceptTypes();
-		$negotiator = new FormatNegotiator();
-		return $negotiator->getBest($this->request['accept'], $allAcceptTypes);
-	}
-
-	public function findByAccept()
-	{
-		$best = $this->findBestAccept();
-		foreach (MediaTypes::all() as $name => $mediaType) {
-			if (in_array($best->getValue(), $mediaType->accept())) {
-				return $mediaType;
-			}
-		}
-		return false;
-	}
-
 	public static function render($request, $response)
 	{
-		$media = new Media($request, $response);
-		$mediaType = $media->findByType() ?: $media->findByAccept();
+		$mediaByType = MediaType::findByType($request['type']);
+		$mediaByAccept = MediaType::findByAccept($request['accept']);
+		$mediaType = $mediaByType ?: $mediaByAccept;
 		if (!$mediaType) {
 			return App::abort(406, 'Unrecognized accept type.');
 		}
--- src/Blainesch/LaravelPrettyController/Http/MediaType.php
+++ src/Blainesch/LaravelPrettyController/Http/MediaType.php
@@ -2,19 +2,48 @@
 
 namespace Blainesch\LaravelPrettyController\Http;
 
+use Negotiation\FormatNegotiator;
+
+MediaType::add('html', [
+	'conditions' => [
+		'accept' => [
+			'text/html',
+			'*/*',
+		],
+	],
+	'encode' => function($request, $response) {
+		$class = strtolower(str_replace('Controller', '', $request['controller']));
+		return \View::make("{$class}.{$request['method']}")->with($response);
+	},
+]);
+
+MediaType::add('json', [
+	'conditions' => [
+		'type' => 'json',
+		'accept' => [
+			'application/json',
+			'application/x-json',
+		],
+	],
+	'encode' => function($request, $response) {
+		return json_encode($response);
+	},
+]);
 
 class MediaType {
 
-	public $options = array();
+	public $options = [];
+
+	public static $types = [];
 
 	public function __construct($options) {
-		$this->options = array_replace_recursive(array(
-			'conditions' => array(
+		$this->options = array_replace_recursive([
+			'conditions' => [
 				'type' => false,
-				'accept' => array(),
-			),
+				'accept' => [],
+			],
 			'encode' => null,
-		), $options);
+		], $options);
 	}
 
 	public function accept()
@@ -30,6 +59,46 @@ class MediaType {
 	public function encode($request, $response)
 	{
 		return call_user_func($this->options['encode'], $request, $response);
+	}
+
+	public static function add($type, $options = [])
+	{
+		$type = static::$types[$type] = new MediaType($options);
+		return $type;
+	}
+
+	public static function all()
+	{
+		return static::$types;
+	}
+
+	public static function findByType($type)
+	{
+		foreach (static::all() as $key => $value) {
+			if ($value->type() === $type) {
+				return $value;
+			}
+		}
+		return false;
+	}
+
+	public static function allAcceptTypes()
+	{
+		return array_reduce(static::all(), function($memo, $value) {
+			return array_merge($memo, $value->accept());
+		}, []);
+	}
+
+	public static function findByAccept($accept)
+	{
+		$negotiator = new FormatNegotiator();
+		$best = $negotiator->getBest($accept, static::allAcceptTypes());
+		foreach (static::all() as $name => $mediaType) {
+			if (in_array($best->getValue(), $mediaType->accept())) {
+				return $mediaType;
+			}
+		}
+		return false;
 	}
 
 }
